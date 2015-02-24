710a8afa366bd0b3ff8e551af7b2b5afc94e4756
Nicola Corti <corti.nico@gmail.com>
2015-02-24 20:44:09 +0100

    Updated compile script

--- compile.sh
+++ compile.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 
+##################
+# Fast compile script
+#  - Just run and read PDF
+#
+# Nic
+##################
+
 cd src
 make all
+mv appunti.pdf ..
+make clean
+cd ..
+evince appunti.pdf
